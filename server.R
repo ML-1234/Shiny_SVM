8596c18ae761ef9be5303d0057626e7645cffb2a
ML-1234 <54897964+ML-1234@users.noreply.github.com>
2019-10-31 18:50:53 +0100

    Matrice en français + organisation server

--- server.R
+++ server.R
@@ -30,41 +30,41 @@ draw_confusion_matrix <- function(cm) {
   layout(matrix(c(1,1,2)))
   par(mar=c(2,2,2,2))
   plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
-  title('CONFUSION MATRIX', cex.main=2)
+  title('MATRICE DE CONFUSION', cex.main=2)
   
-  # create the matrix 
+  # Création de la matrice
   rect(150, 430, 240, 370, col='#3F97D0')
-  text(195, 435, 'Class1', cex=1.2)
+  text(195, 435, 'Pas de défaut', cex=1.2)
   rect(250, 430, 340, 370, col='#F7AD50')
-  text(295, 435, 'Class2', cex=1.2)
-  text(125, 370, 'Predicted', cex=1.3, srt=90, font=2)
-  text(245, 450, 'Actual', cex=1.3, font=2)
+  text(295, 435, 'Défaut', cex=1.2)
+  text(125, 370, 'Prédiction', cex=1.3, srt=90, font=2)
+  text(245, 450, 'Observation', cex=1.3, font=2)
   rect(150, 305, 240, 365, col='#F7AD50')
   rect(250, 305, 340, 365, col='#3F97D0')
-  text(140, 400, 'Class1', cex=1.2, srt=90)
-  text(140, 335, 'Class2', cex=1.2, srt=90)
+  text(140, 400, 'Pas de défaut', cex=1.2, srt=90)
+  text(140, 335, 'Défaut', cex=1.2, srt=90)
   
-  # add in the cm results 
+  # Ajout des informations de la matrice 
   res <- as.numeric(cm$table)
   text(195, 400, res[1], cex=1.6, font=2, col='white')
   text(195, 335, res[2], cex=1.6, font=2, col='white')
   text(295, 400, res[3], cex=1.6, font=2, col='white')
   text(295, 335, res[4], cex=1.6, font=2, col='white')
   
-  # add in the specifics 
+  # Ajout des détails
   plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETAILS", xaxt='n', yaxt='n')
-  text(10, 85, names(cm$byClass[1]), cex=1.2, font=2)
+  text(10, 85, "Sensitivité", cex=1.2, font=2)
   text(10, 70, round(as.numeric(cm$byClass[1]), 3), cex=1.2)
-  text(30, 85, names(cm$byClass[2]), cex=1.2, font=2)
+  text(30, 85, "Spécificité", cex=1.2, font=2)
   text(30, 70, round(as.numeric(cm$byClass[2]), 3), cex=1.2)
-  text(50, 85, names(cm$byClass[5]), cex=1.2, font=2)
+  text(50, 85, "Précision", cex=1.2, font=2)
   text(50, 70, round(as.numeric(cm$byClass[5]), 3), cex=1.2)
   text(70, 85, names(cm$byClass[6]), cex=1.2, font=2)
   text(70, 70, round(as.numeric(cm$byClass[6]), 3), cex=1.2)
   text(90, 85, names(cm$byClass[7]), cex=1.2, font=2)
   text(90, 70, round(as.numeric(cm$byClass[7]), 3), cex=1.2)
   
-  # add in the accuracy information 
+  # Ajout de l'Accuracy et Kappa
   text(30, 35, names(cm$overall[1]), cex=1.5, font=2)
   text(30, 20, round(as.numeric(cm$overall[1]), 3), cex=1.4)
   text(70, 35, names(cm$overall[2]), cex=1.5, font=2)
@@ -92,6 +92,26 @@ shinyServer(function(input, output) {
     }
   )
 
+  
+# SVM 
+  
+  output$m_svm <- renderPlot({
+    svm_model <- svm(form, data=train_ub, tpe="C-classification", kernel ="linear", scale=F)
+    pred_test <- predict(svm_model, test)
+    cmtrx <- confusionMatrix(test$Class, pred_test)
+    draw_confusion_matrix(cmtrx)
+    
+  })
+  
+# RandomForest
+  
+  output$selected_mtry <- renderText({ 
+    paste( "Vous avez choisi le nombre de feuilles égales à", input$mtry, "et un nombre d'arbres égal à", input$ntree,".")
+  })
+  
+  
+# KNN
+  
   output$txtknn<- renderText({ 
     paste( "Vous avez choisi un nombre de voisins égales à", input$k,".")
   })
@@ -105,17 +125,9 @@ shinyServer(function(input, output) {
     draw_confusion_matrix(cmknn)
   })
   
-  output$selected_mtry <- renderText({ 
-    paste( "Vous avez choisi le nombre de feuilles égales à", input$mtry, "et un nombre d'arbres égal à", input$ntree,".")
-  })
+# Gradient Boosting
   
-  
-  output$m_svm <- renderPlot({
-    svm_model <- svm(form, data=train_ub, tpe="C-classification", kernel ="linear", scale=F)
-    pred_test <- predict(svm_model, test)
-    cmtrx <- confusionMatrix(test$Class, pred_test)
-    draw_confusion_matrix(cmtrx)
-    
-  })
+# Régression linéaire
+
   
 })
